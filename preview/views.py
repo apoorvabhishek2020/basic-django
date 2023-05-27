21996b94081aa88791c62d8347762a1cbd343dae
apoorvabhishek2020 <apoorvabhishek2020@gmail.com>
2023-05-27 23:24:08 +0530

    Basic Working Django Application

--- preview/views.py
+++ preview/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-    # return HttpResponse("<h1><strong>SORRY TUNT</strong></h1>")
-    return render(request, 'preview/home.html', {})
+    context = {
+        "title": "Basic Django App",
+        "top_header": "Basic Django Application",
+        "body_header": "Django Application",
+        "body_description": "This is the Basic Django Application for testing workflows",
+    }
+    return render(request, 'preview/home.html', context)
